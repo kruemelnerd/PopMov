b4e87f555bd98b27c124da4594f3a50e9e6291f0
philipp.veit@edicos.de <philipp.veit@edicos.de>
2018-02-17 17:58:58 +0100

    Inital Commit

---
+++ app/src/main/java/de/philippveit/popmov/MainActivity.java
@@ -0,0 +1,66 @@
+package de.philippveit.popmov;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.philippveit.popmov.data.Movie;
+import de.philippveit.popmov.view.MovieAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+    private List<Movie> movieList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.mainActivity_recycler_view);
+
+        movieList = new ArrayList<>();
+        movieAdapter = new MovieAdapter(this, movieList);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(movieAdapter);
+
+        prepareMovies();
+    }
+
+
+    private void prepareMovies(){
+
+        Movie movie = new Movie();
+        movie.setTitle("Interstellar");
+        movieList.add(movie);
+
+        movie = new Movie();
+        movie.setTitle("Toy Story");
+        movieList.add(movie);
+
+        movie = new Movie();
+        movie.setTitle("Toy Story 2");
+        movieList.add(movie);
+
+        movie = new Movie();
+        movie.setTitle("Black Panther");
+        movieList.add(movie);
+
+        movie = new Movie();
+        movie.setTitle("Ip Man");
+        movieList.add(movie);
+
+        movie = new Movie();
+        movie.setTitle("Furios 7");
+        movieList.add(movie);
+    }
+}
---
+++ app/src/main/java/de/philippveit/popmov/view/MovieAdapter.java
@@ -0,0 +1,63 @@
+package de.philippveit.popmov.view;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.philippveit.popmov.R;
+import de.philippveit.popmov.data.Movie;
+
+/**
+ * Created by pveit on 17.02.2018.
+ */
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder> {
+
+
+    private Context mContext;
+    private List<Movie> movieList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        public ImageView thumbnail;
+
+        public MyViewHolder(View view){
+            super(view);
+            title = (TextView) view.findViewById(R.id.movie_title_tv);
+            thumbnail = (ImageView) view.findViewById(R.id.movie_thumbnail_iv);
+        }
+    }
+
+    public MovieAdapter (Context context, List<Movie> movies){
+        this.mContext = context;
+        this.movieList = movies;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_card, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Movie movie = movieList.get(position);
+        holder.title.setText(movie.getTitle());
+        Picasso.with(mContext).load("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg").into(holder.thumbnail);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+
+}
